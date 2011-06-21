ec980f2f561013704d9c92116ccf8fecaeb4e16b
Michal Špaček <tupinek@gmail.com>
2011-06-21 14:34:20 +0200

    Add doc stub. Sort constructor parameters. Improve default values.

--- Example.pm
+++ Example.pm
@@ -18,14 +18,14 @@ sub new {
 	my ($class, @params) = @_;
 	my $self = bless {}, $class;
 
+	# Interactive.
+	$self->{'interactive'} = 0;
+
 	# Print.
-	$self->{'print'} = undef;
+	$self->{'print'} = 0;
 
 	# Run.
 	$self->{'run'} = 1;
-
-	# Interactive.
-	$self->{'int'} = 0;
 
 	# Process params.
 	set_params($self, @params);
@@ -73,3 +73,99 @@ sub _get_content {
 }
 
 1;
+
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+App::Pod::Example - Base class for pod_example script.
+
+=head1 SYNOPSIS
+
+ use App::Pod::Example;
+ my $app = App::Pod::Example->new(%parameters);
+ $app->run($file, $section);
+
+=head1 METHODS
+
+=over 8
+
+=item C<new(%parameters)>
+
+ Constructor.
+
+=over 8
+
+=item * C<interactive>
+
+ TODO
+ Default value is 0.
+
+=item * C<print>
+
+ TODO
+ Default value is 0.
+
+=item * C<run>
+
+ TODO
+ Default value is 1.
+
+=back
+
+=item C<run($file, $section)>
+
+TODO
+
+=back
+
+=head1 ERRORS
+
+ Mine:
+         TODO
+
+ From Class::Utils::set_params():
+         Unknown parameter '%s'.
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use App::Pod::Example;
+
+ # Run.
+ # TODO
+ App::Pod::Example->new->run();
+
+=head1 DEPENDENCIES
+
+L<Class::Utils(3pm)>,
+L<English(3pm)>,
+L<Error::Pure(3pm)>,
+L<Pod::Abstract(3pm)>.
+
+=head1 SEE ALSO
+
+TODO
+
+=head1 AUTHOR
+
+Michal Špaček L<skim@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD license.
+
+=head1 VERSION
+
+0.01
+
+=cut

297f575ec9adfa9e135dc4e0551ce789777d84b6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-06-28 18:20:44 +0200

    Prepare for 0.23 version

--- Example.pm
+++ Example.pm
@@ -18,7 +18,7 @@ Readonly::Scalar my $EMPTY_STR => q{};
 Readonly::Scalar my $HASH => q{#};
 Readonly::Scalar my $SPACE => q{ };
 
-our $VERSION = 0.22;
+our $VERSION = 0.23;
 
 # Constructor.
 sub new {
@@ -231,6 +231,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.22
+0.23
 
 =cut
